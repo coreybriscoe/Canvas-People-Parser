5b9f86db36a31ad4d4a42bae1cec55dd3b2abf1a
corbrisc <corbrisc@iu.edu>
2023-01-25 14:20:26 -0500

    Add logic to find names of all course members. Co-authored by: coreybriscoe <coreybriscoe@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-// load a file from the file system
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 // read from ../in/file.txt
 const contents = readFileSync(resolve(__dirname, '../in/file.txt'), 'utf8');
-// console.log(contents);
 
-// extract the each instance of text in an a tag with the class "roster_user_name student_context_card_trigger" with arbitrary properties before or after the class attribute
+// a simple tr regex. Each individual (student or instructor) is one row in the table
+const rowRegex = /<tr[^>]*>[\s\S]+?(?=<\/tr>)<\/tr>/g;
+const rowMatch = contents.match(rowRegex);
 
-
-
-const regex = /<a [^>]*class="roster_user_name student_context_card_trigger" [^>]*>([^<]*)<\/a>/g;
-// list patterns that match the above:
-
-
-// regex to match any character (including newline) except the exact sequence </a>
-
-
-const match = regex.exec(contents);
-console.log(match);
-
-// how to accept zero to many characters in a regex?
+if (!rowMatch) throw new Error('No rows found');
+for (const row of rowMatch) {
+	// extract the each instance of text in an a tag with the class "roster_user_name student_context_card_trigger" with arbitrary properties before or after the class attribute
+	const nameRegex = /<a [^>]*class="roster_user_name student_context_card_trigger"[^>]*>[\s\S]+?(?=<\/a>)<\/a>/g;
+	const nameMatch = row.match(nameRegex);
+	if (!nameMatch) throw new Error('No name found');
+	// extract from nameMatch the text between the a tags
+	const name = nameMatch[0].replace(/<a [^>]*>/, '').replace(/<\/a>/, '');
+	console.log(name);
+}
