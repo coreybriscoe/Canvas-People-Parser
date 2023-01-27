a4455747e9ee14506b9f3f7f833381b9b99a2cd6
coreybriscoe <coreybriscoe@gmail.com>
2023-01-26 23:48:05 -0500

    add logic for assigning students, output. Program mostly functional!

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ import { GradingAssignmentsData, Section, Instructor, Student } from './types';
 const contents = readFileSync(resolve(__dirname, '../in/file.txt'), 'utf8');
 
 // a simple tr regex. Each individual (student or instructor) is one row in the table
+// This will include the header row, so we slice it off later
 const rowRegex = /<tr[^>]*>[\s\S]+?(?=<\/tr>)<\/tr>/g;
 const rowMatch = contents.match(rowRegex);
 
 if (!rowMatch) throw new Error('No rows found');
 
-// get user input at runtime
+// get user input options at runtime
 const prompt = require('prompt-sync')({sigint: true});
 const useLabSectionResponse = prompt('Prioritize assigning students to their lab section instructors? (y/n): ');
 const useLabSection = useLabSectionResponse === 'y';
@@ -24,7 +25,7 @@ if (useLabSection) {
 	for (const sectionName of labSectionNames) {
 		const section = new Section(sectionName);
 		while (true) {
-			const taName = prompt(`Enter one name of the TA(s) for ${section}, or enter for next section: `);
+			const taName = prompt(`Enter one name of the TA(s) for ${section.sectionId}, or enter for next section: `);
 			if (taName) {
 				section.instructors.push(new Instructor(taName))
 			} else break;
@@ -33,14 +34,13 @@ if (useLabSection) {
 		data.sections.push(section);
 	}
 } else {
-	console.log('Functionality not supported yet.');
+	console.warn('Functionality not supported yet.');
 	process.exit(0);
 }
-for (const row of rowMatch) {
+for (const row of rowMatch.slice(1)) {
 	const rowContents = row.split('</td>');
-	// column 1 is the profile picture
+	// column 1 is the profile picture (skip)
 	// column 2 is the name and pronouns, we just want name
-	// 
 	const name = rowContents[1] // first row
 	.split(/<a [^>]*class="roster_user_name student_context_card_trigger"[^>]*>/) // find the a tag Canvas puts around the name
 	[1] // get the name itself
@@ -53,8 +53,43 @@ for (const row of rowMatch) {
 	const username = rowContents[2].split('>')[1].split('<')[0].trim();
 
 	// column 4 is the sections
-	const sections = rowContents[3].split(`${data.sectionPrefix}`).slice(1).map(section => section.split('</div>')[0].trim()).filter(section => data.sections.map(labSections => labSections.sectionId).includes(section));
-	console.log(sections);
+	const sections = rowContents[3] // third row
+	.split(`${data.sectionPrefix}`) // split by the section prefix
+	.slice(1) // exclude anything before first prefix
+	.map(section => section.split('</div>')[0].trim()) // trim everything after section name
+	.filter(section => data.sections.map(labSections => labSections.sectionId).includes(section)); // filter out sections that aren't lab sections
 	if (!sections) throw new Error('No sections found');
-	if (sections.length > 1) throw new Error('More than one section found');
+	if (sections.length > 1) console.warn(`WARNING: More than one section found for student ${name}`);
+
+	// column 5 is the role, which may be {Student, TA, Teacher, Instructor-Added Student, ...}, and appears in divs
+	const role = rowContents[4].split('<div>')[1].split('</div>')[0].trim();
+	if (!role) throw new Error('No role found');
+	if (!role.includes("Student")) continue;
+
+	const student = new Student(name, username, '', sections[0]);
+	// add student to instructor in section with least students
+	// get the Section object corresponding to the student's section
+	const section = data.sections.filter((sec: Section) => sec.sectionId === sections[0])[0];
+	try {
+		const instructorToAddTo = data.sections.filter((sec: Section) => sec.sectionId === sections[0])[0]
+	.instructors.reduce((prev: Instructor, curr: Instructor) => prev.assignees.length < curr.assignees.length ? prev : curr);
+	instructorToAddTo.assignees.push(student);
+	} catch (e) {
+		console.log(e, student.name, student.section);
+	}
+	
 }
+
+console.log(data);
+
+// display output
+data.sections.forEach((section: Section) => {
+	console.log(`==========${section.sectionId}==========`);
+	section.instructors.forEach((instructor: Instructor) => {
+		console.log(`${instructor.name}'s students:`);
+		instructor.assignees.forEach((student: Student) => {
+			console.log(student.name + ' ' + student.id);
+		});
+		console.log();
+	});
+});
