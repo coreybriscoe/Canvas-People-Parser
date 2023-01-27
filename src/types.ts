fd9e37b4e86dbef69ca49482835f41d27faef220
coreybriscoe <coreybriscoe@gmail.com>
2023-01-26 22:25:47 -0500

    add constructors to classes

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,31 @@
 export class GradingAssignmentsData {
-	"section-prefix": string;
+	"sectionPrefix": string;
 	"sections": Section[];
+
+	constructor(sectionPrefix) {
+		this.sectionPrefix = sectionPrefix;
+		this.sections = [];
+	}
 }
 
 export class Section {
-	"section-id": string;
+	"sectionId": string;
 	"instructors": Instructor[];
+
+	constructor(sectionId) {
+		this.sectionId = sectionId;
+		this.instructors = [];
+	}
 }
 
 export class Instructor {
 	"name": string;
 	"assignees": Student[];
+
+	constructor(name) {
+		this.name = name;
+		this.assignees = [];
+	}
 }
 
 export class Student {
@@ -18,4 +33,11 @@ export class Student {
 	"id": string;
 	"pronouns": string;
 	"section": string;
+
+	constructor(name, id, pronouns, section) {
+		this.name = name;
+		this.id = id;
+		this.pronouns = pronouns;
+		this.section = section;
+	}
 }
